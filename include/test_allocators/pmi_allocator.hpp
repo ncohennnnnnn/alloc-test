e56b0a79b52ae8e5f7e90984413051880605f305
John Biddiscombe <biddisco@cscs.ch>
2023-11-29 14:49:08 +0100

    Fix build to work with unit-tesst branch of pmialloc

--- include/test_allocators/pmi_allocator.hpp
+++ include/test_allocators/pmi_allocator.hpp
@@ -1,16 +1,16 @@
 #pragma once
 
-#include <allocator.hpp>
-
 #include <cstdint>
 #include <string>
+//
+#include <pmimalloc/allocator.hpp>
 
 namespace alloc_test {
 
-template<typename T>
-auto make_test_allocator() 
-{
-/*
+    template <typename T>
+    auto make_test_allocator()
+    {
+        /*
 Minimum size for mem : max( 30 , 29 + ceil(log_2(nb_threads)) )
  ____________________________________________
 |threads | min size      threads | min size  |
@@ -30,14 +30,14 @@ Minimum size for mem : max( 30 , 29 + ceil(log_2(nb_threads)) )
 
 Maximum size for mem : 35 (max of mmap on my machine)
 */
-    std::size_t mem = 1ull << 32;
-    using res_t   = resource <context <not_pinned <host_memory <base>> , backend_none> , ex_mimalloc> ;
-    using alloc_t = pmimallocator<T, res_t>;
-    auto res = std::make_shared<res_t>(mem);
-    alloc_t a{res};
-    return a;
-}
+        std::size_t mem = 1ull << 32;
+        using res_t = resource<context<not_pinned<host_memory<base>>, backend_none>, ext_mimalloc>;
+        using alloc_t = pmimallocator<T, res_t>;
+        auto res = std::make_shared<res_t>(mem);
+        alloc_t a{res};
+        return a;
+    }
 
-inline constexpr const char* allocator_name = "pmi_malloc";
+    inline constexpr const char* allocator_name = "pmi_malloc";
 
-} // namespace alloc_test
+}    // namespace alloc_test
